7ab09deb72122301a34382ae42dba5d470c86a26
turtle-nest <n78693107@gmail.com>
2025-05-28 13:07:59 +0200

    6. Creating functions specific to employees

--- Typescript/task_2/js/main.ts
+++ Typescript/task_2/js/main.ts
@@ -48,3 +48,21 @@ export function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200));     // Teacher
 console.log(createEmployee(1000));    // Director
 console.log(createEmployee('$500'));  // Director
+
+// Predicate type to know if the employee is a Director
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Function that executes the method based on the role
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+// Tests
+console.log(executeWork(createEmployee(200)));  // Getting to work
+console.log(executeWork(createEmployee(1000))); // Getting to director tasks
